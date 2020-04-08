3af57906fb7a35dbf66bc34f8bc671cf225adb04
SebTota <kidokyo1@gmail.com>
2020-04-08 12:00:39 -0400

    Added api call function to get an array of classes

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+//---API CALLS---//
+function genClassArrStr(classes) {
+    var classUrl = ("cls=") + classes[0];
+
+    for (let cls = 1; cls < classes.length; cls++) {
+        classUrl = classUrl + ("&cls=") + classes[cls];
+    }
+    return classUrl;
+}
+
+function getClasses(classes) {
+    let apiUrl = "http://3.21.207.37:3000/classes?" + genClassArrStr(classes);
+    console.log(apiUrl);
+
+    fetch(apiUrl).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        return(data);
+    }).catch(function() {
+        console.log("Error making api call");
+        return -1;
+    });
+
+
+}
 
 
 //---ACTION LISTENERS---//
@@ -6,9 +31,9 @@ document.getElementById('btnFindSchedules').addEventListener("click", function()
     classIn = classIn.replace(/\s/g, ""); // Remove all empty spaces from input
     var classes = classIn.split(","); // Tokenize classes based on ','
 
-    // For testing only, remove in production
-    for (var i = 0; i < classes.length; i++) {
-        console.log(classes[i]);
-    }
+    console.log(getClasses(classes));
+
+
 
 });
+//---END ACTION LISTENERS---//
